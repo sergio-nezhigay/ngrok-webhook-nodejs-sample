4dc1b36ed6ee1447c4be3a7890aef64049d11339
Sergio Nezhigay <abc1971h@gmail.com>
2024-06-17 16:00:25 +0300

    init

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-var express = require('express');
+var express = require("express");
+var crypto = require("crypto");
 var app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
+const SHOPIFY_SECRET = "40334744528540f47545ed90a261788c"; // Replace with your actual Shopify secret
 
-app.all('/*', function (req, res) {
-   console.log("-------------- New Request --------------");
-   console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
-   console.log("Body:"+ JSON.stringify(req.body, null, 3));
-   res.json({ message: "Thank you for the message" });
-})
+// Middleware to verify Shopify HMAC
+function verifyShopifyWebhook(req, res, next) {
+  console.log("🚀 ~ verifyShopifyWebhook:");
+  const hmac = req.get("x-shopify-hmac-sha256");
+  const body = JSON.stringify(req.body);
+
+  const hash = crypto
+    .createHmac("sha256", SHOPIFY_SECRET)
+    .update(body, "utf8")
+    .digest("base64");
+
+  //  if (true) {
+  if (hash === hmac) {
+    console.log("hash === hmac");
+    next(); // HMAC is valid
+  } else {
+    console.log("hash !== hmac!!");
+    next(); // emulate HMAC is valid
+    //res.status(401).send("HMAC verification failed");
+  }
+}
+
+app.all("/*", verifyShopifyWebhook, function (req, res) {
+  console.log("-------------- New Request --------------");
+  console.log("Headers:" + JSON.stringify(req.headers, null, 3));
+  console.log("Body:" + JSON.stringify(req.body, null, 3));
+  res.json({ message: "Thank you for the message" });
+});
 
 app.listen(port, function () {
-   console.log(`Example app listening at ${port}`)
-})
+  console.log(`Example app listening at ${port}`);
+});
